2ad802d0dcac05655afa4510234062e37abf329e
jeff <geoffreyerastus956@gmail.com>
2024-01-22 09:56:41 -0500

    pushPop

--- stack.py
+++ stack.py
@@ -1,5 +1,24 @@
-#fifo
-# push
-# propertypepe or top
+#lifo
+# push  -add
+#pop - remove top
+# peek or top
 # is empty
+#not inbuilt
 
+#implemented using list and modules
+
+#push ---> append add element to the top of the list
+#pop ---> remove the element from the top of the list
+
+stack = []
+stack.append(10)
+stack.append(15)
+stack.append(20)
+stack.append(25)
+print(stack)
+
+stack.pop()
+print(stack)
+
+stack.pop()
+print(stack)
